3dde75fa1feda9696aee5c3cc4a3d294a55c17f4
Wale Fagbodun <adewalefagbodun@gmail.com>
2024-02-05 00:55:56 +0000

    6 files for this monty task

---
+++ nop.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+  *f_nop- does nothing for this code
+  *@head: stack head for this code
+  *@counter: line_number for this code
+  *
+  *Return: no return
+ */
+
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
